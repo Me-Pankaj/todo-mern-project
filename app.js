f0030915a6d4ba6288607625e8dd519d658fd9f6
Me-Pankaj <118957086+Me-Pankaj@users.noreply.github.com>
2023-04-13 19:33:48 +0530

    first

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["https://react-todo-me-pankaj.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
